a6752759491db2c899630491f4fa820b31c29eed
neethiwin <neethiwin@users.noreply.github.com>
2019-12-14 19:20:55 +0530

    Create mixinsandinhertience.dart

---
+++ mixinsandinhertience.dart
@@ -0,0 +1,33 @@
+import 'package:Dart/Honda.dart';
+
+void main(){
+  var honda = Honda();
+  honda.carNo = 'Ka01H12';
+  print(honda.carNo);
+  print(honda.getFuel());
+  honda.start();
+}
+
+class CNGKit {
+  String getFuel(){
+    return 'CNG fuel';
+  }
+}
+
+class Car {
+  var _carno;
+
+  set carNo(value) => _carno = value;
+  String get carNo => _carno;
+
+  String getFuel(){
+    return 'general fuel';
+  }
+}
+
+class Honda extends Car with CNGKit{
+  void start(){
+    print('honda car started');
+  }
+
+}
